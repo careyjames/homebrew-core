0b57c7ef1e1e139b54d7c4b5119d02680476304c
Carey Balboa <carey.balboa@machelpnashville.com>
2023-10-10 22:46:02 -0500

    dns-scout 5.8 (new formula)

---
+++ Formula/d/dns-scout.rb
@@ -0,0 +1,13 @@
+class DnsScout < Formula
+  desc "Pulls and display DNS records"
+  homepage "https://github.com/careyjames/DNS-Scout"
+  url "https://github.com/careyjames/DNS-Scout/archive/refs/tags/v5.8.tar.gz"
+  sha256 "2ac687dfdd7f218a86ae5235c05f63c785af00786366e26f90d600972a5b282a"
+  license "MIT"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", *std_go_args(ldflags: "-s -w")
+  end
+end
