06a1363d68afe5833663cae97df8ec0d8f13a233
Carey Balboa <52982329+careyjames@users.noreply.github.com>
2023-10-11 16:00:01 -0500

    Update dns-scout.rb
    45b5fe937a570319da3b2957681010adcf4b077cc8bad59e5a1f445e18a951ab dns-scout-macos-intel
    
    f8e2c2f68c2e402b8aeef0225ebf04aedb6c6852b102e2c7f3dbe6f6f1e60ec9 dns-scout-macos-silicon

--- Formula/d/dns-scout.rb
+++ Formula/d/dns-scout.rb
@@ -2,7 +2,8 @@ class DnsScout < Formula
   desc "Pulls and display DNS records"
   homepage "https://github.com/careyjames/DNS-Scout"
   url "https://github.com/careyjames/DNS-Scout/archive/refs/tags/v5.8.tar.gz"
-  sha256 "2ac687dfdd7f218a86ae5235c05f63c785af00786366e26f90d600972a5b282a"
+  sha256 "f8e2c2f68c2e402b8aeef0225ebf04aedb6c6852b102e2c7f3dbe6f6f1e60ec9"
+  sha256 "45b5fe937a570319da3b2957681010adcf4b077cc8bad59e5a1f445e18a951ab"
   license "MIT"
 
   depends_on "go" => :build
